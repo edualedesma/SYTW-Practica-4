d3fbe5bb00cf6966f63cf5b576150a02b9b86d18
Eduardo <eaculed@gmail.com>
2014-10-20 18:15:36 +0100

    Añadiendo vistas y funcionalidad inicial

---
+++ app.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'uri'
+require 'pp'
+#require 'socket'
+require 'data_mapper'
+
+DataMapper.setup( :default, ENV['DATABASE_URL'] || 
+                            "sqlite3://#{Dir.pwd}/my_shortened_urls.db" )
+DataMapper::Logger.new($stdout, :debug)
+DataMapper::Model.raise_on_save_failure = true 
+
+require_relative 'model'
+
+DataMapper.finalize
+
+#DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
+
+Base = 36
+
+get '/' do
+  puts "inside get '/': #{params}"
+  @list = ShortenedUrl.all(:order => [ :id.asc ], :limit => 20)
+  # in SQL => SELECT * FROM "ShortenedUrl" ORDER BY "id" ASC
+  haml :index
+end
+
+post '/' do
+  puts "inside post '/': #{params}"
+  uri = URI::parse(params[:url])
+  if uri.is_a? URI::HTTP or uri.is_a? URI::HTTPS then
+    begin
+      @short_url = ShortenedUrl.first_or_create(:url => params[:url])
+    rescue Exception => e
+      puts "EXCEPTION!!!!!!!!!!!!!!!!!!!"
+      pp @short_url
+      puts e.message
+    end
+  else
+    logger.info "Error! <#{params[:url]}> is not a valid URL"
+  end
+  redirect '/'
+end
+
+get '/:shortened' do
+  puts "inside get '/:shortened': #{params}"
+  short_url = ShortenedUrl.first(:id => params[:shortened].to_i(Base))
+
+  # HTTP status codes that start with 3 (such as 301, 302) tell the
+  # browser to go look for that resource in another location. This is
+  # used in the case where a web page has moved to another location or
+  # is no longer at the original location. The two most commonly used
+  # redirection status codes are 301 Move Permanently and 302 Found.
+  redirect short_url.url, 301
+end
+
+error do haml :index end
+
