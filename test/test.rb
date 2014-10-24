0fdd591be088c6a2ff7d330b1ad4acd03b1ed69c
Sergio <alu0100696615@ull.edu.es>
2014-10-24 17:13:19 +0100

    Añadido before all, antes de las pruebas

--- test/test.rb
+++ test/test.rb
@@ -14,7 +14,16 @@ def app
 end
 
 describe 'Tests de app.rb' do
-
+     before :all do
+        @Pagina = "http://www.diariodeavisos.com"
+	@Pagina2 = "http://www.eldia.es"
+	@ShortPagina = "periodidco"
+	@Objeto = ShortenedUrl.first_or_create(:url => "http://www.diariodeavisos.com", :myurl =>'periodico', :username => 'Edu')
+	@ObjetoDist = ShortenedUrl.first(:username=> 'Juan')
+	
+     end
+     
+     
    
    it "Comprobar que va a la index" do
 	  get '/'
